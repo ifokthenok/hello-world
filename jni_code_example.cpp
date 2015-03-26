d068a43f55a84af37e1ac3246dbfd2736e13af8a
hao <ifokthenok@gmail.com>
2015-03-27 01:12:43 +0800

    add test samples and jnichecker.py

---
+++ jni_code_example.cpp
@@ -0,0 +1,10 @@
+#include "class_data.h"
+jclass Java_example_package_Cls_f(JNIEnv* env, jobject obj, jstring str) {
+	void* buf;
+	size_t bufLen;
+	const char* name = env->GetStringUTFChars(str, 0);
+	getClassData(name, buf, bufLen); /*Get the class bytes by name*/
+	return env->DefineClass(name, obj, buf, bufLen);
+} /*
+
+
